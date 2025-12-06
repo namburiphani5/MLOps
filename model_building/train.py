0184e7a5077fbb76845ef3f3e92056ee40179648
namburiphani5 <57583444+namburiphani5@users.noreply.github.com>
2025-12-06 19:19:15 +0530

    Update train.py

--- model_building/train.py
+++ model_building/train.py
@@ -134,7 +134,7 @@ with mlflow.start_run():
     print(f"Model saved as artifact at: {model_path}")
 
     # Upload to Hugging Face
-    repo_id = "{hf_token}/churn-model"
+    repo_id = "namburiphani5/churn-model"
     repo_type = "model"
 
     # Step 1: Check if the space exists
