214088cb77e5d6f364fb74174b4e603bfcb0850c
namburiphani5 <57583444+namburiphani5@users.noreply.github.com>
2025-12-06 19:28:01 +0530

    Update hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -4,8 +4,8 @@ import os
 hf_token = os.getenv("HF_TOKEN")
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="mlops/deployment",     # the local folder containing your files
-    repo_id="{hf_token}/Bank-Customer-Churn",          # the target repo
+    folder_path="deployment",     # the local folder containing your files
+    repo_id="namburiphani5/Bank-Customer-Churn",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
